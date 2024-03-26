5e962ac27f10fb34961c0514c0ab3f71d4c48600
alexmerlin <alex.merlin.1985@gmail.com>
2024-03-26 09:37:20 +0200

    Added script components.
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

---
+++ github-actions/docs/README.md
@@ -0,0 +1,141 @@
+# DotKernel GitHub Actions: docs
+
+This action can be used to build and deploy documentation for a DotKernel repository.
+
+The action makes several assumptions:
+
+- You want to use the [dotkernel/documentation-theme](https://github.com/dotkernel/documentation-theme).
+- You will build and deploy your docs in the gh-pages branch.
+- You will build your docs in the `docs/html` directory (though this is configurable).
+
+If no `mkdocs.yml` file is available in the repository, the action aborts early
+with no errors.
+
+As an example:
+
+```yaml
+- name: Docs
+  uses: dotkernel/documentation-theme/github-actions/docs@master
+  with:
+    emptyCommits: false
+    siteUrl: https://docs.laminas.dev
+```
+
+The above will build docs in the `docs/html` subdirectory, will NOT build for
+empty commits, and uses an alternate base site URL.
+
+## Behaviors
+
+- By default, the action auto-determines the `siteUrl` based on the repository,
+  matching against the repository organization.
+
+- By default, it will build even when the commit is empty.
+
+## Options
+
+- emptyCommits: Specify whether to build documentation for empty commits.
+  Defaults to `true`.
+
+- username: Specify a git username under which to make the documentation commit.
+  If none is specified, it derives it from the user who made the commit that
+  triggered the action.
+
+- useremail Specify a git user email under which to make the documentation commit.
+  If none is specified, it uses `${username}@users.noreply.github.com`.
+
+- siteUrl: Specify the scheme and authority of the site URL under which
+  documentation will be available. By default, this will be either
+  https://docs.laminas.dev or https://docs.mezzio.dev as derived from the
+  `${GITHUB_REPOSITORY}` value.
+
+## Environment
+
+This action requires one of either the environment variable `$DEPLOY_TOKEN` or `$DOCS_DEPLOY_KEY`:
+
+- **$DEPLOY_TOKEN** (preferred) should be a GitHub OAuth token with permissions to commit code.
+  (In the Laminas and Mezzio organizations, we store this as the `ORGANIZATION_ADMIN_TOKEN` secret at the organization level.)
+
+- **$DOCS_DEPLOY_KEY** (legacy) should be a GitHub deployment key, generated as follows:
+
+  ```bash
+  $ ssh-keygen -t rsa -b 4096 -C "${git config user.email}" -f gh-pages -N ""
+  # Creates the files:
+  # - gh-pages (private SSH key)
+  # - gh-pages.pub (public key)
+  ```
+
+  Once generated, go to the **Settings** tab of your repository.
+  Under **Deploy Keys**, select the **Add Key** button, add the contents of your `gh-pages.pub` file, and select the "Allow Write Access" checkbox.
+  Then go to **Secrets**, select the **Add** button, give the new secret the title `DOCS_DEPLOY_KEY`, and paste in the contents of your `gh-pages` file.
+
+## Example workflow
+
+```yaml
+name: docs-build
+
+on:
+  release:
+    types: [published]
+  repository_dispatch:
+    types: docs-build
+
+jobs:
+  build-deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Build and deploy documentation
+        uses: dotkernel/documentation-theme/github-actions/docs@master
+        env:
+          DEPLOY_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        with:
+          emptyCommits: false
+```
+
+### Legacy workflows
+
+1. Using a `DOCS_DEPLOY_KEY`
+
+   ```yaml
+   name: docs-build
+
+   on:
+     release:
+       types: [published]
+     repository_dispatch:
+       types: docs-build
+
+   jobs:
+     build-deploy:
+       runs-on: ubuntu-latest
+       steps:
+         - name: Build and deploy documentation
+           uses: dotkernel/documentation-theme/github-actions/docs@master
+           env:
+             DEPLOY_DEPLOY_KEY: ${{ secrets.DOCS_DEPLOY_KEY }}
+           with:
+             emptyCommits: false
+   ```
+
+2. If your repository has not yet updated to use release branches and/or [automatic-releases](https://github.com/laminas/automatic-releases), and you are still using the "master" branch to reflect current stable, you may also use the following workflow:
+
+   ```yaml
+   name: docs-build
+
+   on:
+     push:
+       branches:
+         - master
+     repository_dispatch:
+       types: docs-build
+
+   jobs:
+     build-deploy:
+       runs-on: ubuntu-latest
+       steps:
+         - name: Build and deploy documentation
+           uses: dotkernel/documentation-theme/github-actions/docs@master
+           env:
+             DOCS_DEPLOY_KEY: ${{ secrets.DOCS_DEPLOY_KEY }}
+           with:
+             emptyCommits: false
+   ```
---
+++ github-actions/docs/entrypoint.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+# - Based on https://github.com/peaceiris/actions-gh-pages
+#   Copyright (c) 2019 Shohei Ueda (peaceiris)
+# @copyright 2020 Laminas Project
+
+set -e
+
+export TOP_PID=$$
+trap "exit 0" TERM
+# shellcheck disable=SC2173
+trap "exit 1" KILL
+
+function print_error() {
+    echo -e "\e[31mERROR: ${1}\e[m"
+}
+
+function print_info() {
+    echo -e "\e[36mINFO: ${1}\e[m"
+}
+
+function skip() {
+    print_info "Skipping documentation build"
+    kill -s TERM $TOP_PID
+}
+
+function remote_repo_setup() {
+    local -n RESULT=$1
+    local DEPLOY_TOKEN=$2
+    RESULT="https://x-access-token:${DEPLOY_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+}
+
+function remote_repo_setup_legacy() {
+    local -n RESULT=$1
+    local DEPLOY_KEY=$2
+
+    print_info "setup with DOCS_DEPLOY_KEY"
+
+    SSH_DIR="/root/.ssh"
+    mkdir "${SSH_DIR}"
+    ssh-keyscan -t rsa github.com > "${SSH_DIR}/known_hosts"
+    echo "${DEPLOY_KEY}" > "${SSH_DIR}/id_rsa"
+    chmod 400 "${SSH_DIR}/id_rsa"
+
+    # shellcheck disable=SC2034
+    RESULT="git@github.com:${GITHUB_REPOSITORY}.git"
+}
+
+WHAT_DO_WE_BUILD=$(php /usr/bin/do-we-build-and-if-so-what.php)
+export WHAT_DO_WE_BUILD
+if [[ "${WHAT_DO_WE_BUILD}" == "FALSE" ]]; then
+    print_info "Not the most recent tag, or not a manual build request; skipping"
+    kill -s TERM $TOP_PID
+fi
+
+# checkout the repository at the given reference
+git clone "https://github.com/${GITHUB_REPOSITORY}.git" "${GITHUB_WORKSPACE}"
+(cd "${GITHUB_WORKSPACE}" && git checkout "${WHAT_DO_WE_BUILD}")
+
+if [ ! -f "${GITHUB_WORKSPACE}/mkdocs.yml" ];then
+    print_info "No documentation detected; skipping"
+    kill -s TERM $TOP_PID
+fi
+
+# check values
+remote_repo=""
+if [ -n "${DEPLOY_TOKEN}" ]; then
+    remote_repo_setup remote_repo "${DEPLOY_TOKEN}"
+else
+    if [ -n "${DOCS_DEPLOY_KEY}" ]; then
+        remote_repo_setup_legacy remote_repo "${DOCS_DEPLOY_KEY}"
+    else
+        print_error "Neither DEPLOY_TOKEN nor DOCS_DEPLOY_KEY found; please provide one or the other in your workflow env"
+        kill -s KILL $TOP_PID
+    fi
+fi
+print_info "Publishing to ${GITHUB_REPOSITORY}"
+
+site_url=${INPUT_SITE_URL}
+if [[ "${site_url}" == "" ]]; then
+    site_url=https://docs.dotkernel.com/${GITHUB_REPOSITORY#*/}
+fi
+print_info "Using site URL ${site_url}"
+
+PUBLISH_REPOSITORY=${GITHUB_REPOSITORY}
+PUBLISH_BRANCH=gh-pages
+PUBLISH_DIR=$(grep 'site_dir:' mkdocs.yml | awk '{print $2}')
+
+print_info "Deploy to ${PUBLISH_REPOSITORY}@${PUBLISH_BRANCH} from directory ${PUBLISH_DIR}"
+
+print_info "Cloning documentation theme"
+git clone https://github.com/dotkernel/documentation-theme.git "${GITHUB_WORKSPACE}/documentation-theme"
+
+print_info "Building documentation"
+(cd "${GITHUB_WORKSPACE}" ; ./documentation-theme/build.sh -u "${site_url}")
+
+print_info "Deploying documentation"
+remote_branch="${PUBLISH_BRANCH}"
+local_dir="${HOME}/ghpages_${RANDOM}"
+
+if git clone --depth=1 --single-branch --branch "${remote_branch}" "${remote_repo}" "${local_dir}"; then
+    print_info "- Cloning branch ${remote_branch} from ${GITHUB_REPOSITORY} to ${local_dir} and removing previous files"
+    cd "${local_dir}"
+
+    git rm -r --ignore-unmatch '*'
+else
+    print_info "- Creating new ${remote_branch} branch on ${GITHUB_REPOSITORY} in ${local_dir}"
+    git clone --depth=1 --single-branch "${remote_repo}" "${local_dir}"
+    cd "${local_dir}"
+    git checkout --orphan "${remote_branch}"
+    git rm -rf .
+fi
+
+find "${GITHUB_WORKSPACE}/${PUBLISH_DIR}" -maxdepth 1 -not -name ".git" -not -name ".github" | \
+    tail -n +2 | \
+    xargs -I % cp -rf % "${local_dir}/"
+
+print_info "- Adding user and email to local clone for purposes of commit"
+if [[ -n "${INPUT_USERNAME}" ]]; then
+    cd "${local_dir}" && git config user.name "${INPUT_USERNAME}"
+else
+    cd "${local_dir}" && git config user.name "${GITHUB_ACTOR}"
+fi
+if [[ -n "${INPUT_USEREMAIL}" ]]; then
+    cd "${local_dir}" && git config user.email "${INPUT_USEREMAIL}"
+else
+    cd "${local_dir}" && git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+fi
+(cd "${local_dir}" && git remote rm origin) || true
+cd "${local_dir}" && git remote add origin "${remote_repo}"
+cd "${local_dir}" && git add --all
+
+print_info "Allowing empty commits: ${INPUT_EMPTYCOMMITS}"
+COMMIT_MESSAGE="Automated deployment: $(date -u) ${WHAT_DO_WE_BUILD}"
+if [[ ${INPUT_EMPTYCOMMITS} == "false" ]]; then
+    (cd "${local_dir}" && git commit -m "${COMMIT_MESSAGE}") || skip
+else
+    cd "${local_dir}" && git commit --allow-empty -m "${COMMIT_MESSAGE}"
+fi
+
+# push to publishing branch
+print_info "- Pushing from ${local_dir} to ${remote_branch}"
+cd "${local_dir}" && git push origin "${remote_branch}"
+
+print_info "${WHAT_DO_WE_BUILD} was successfully deployed"
